417937692d193167253b6d52ec6e60dfaed755e6
Frostmourne <cozroman@yandex.ru>
2020-03-19 20:02:51 +0300

    feat: add a room card

--- src/js/index.js
+++ src/js/index.js
@@ -6,9 +6,14 @@ import 'bootstrap-datepicker/dist/css/bootstrap-datepicker3.min.css';
 import 'bootstrap-datepicker/dist/css/bootstrap-datepicker3.standalone.css';
 import 'daterangepicker/daterangepicker';
 import 'daterangepicker/daterangepicker.css';
+import 'bxslider/dist/jquery.bxslider.min';
+import 'bxslider/dist/jquery.bxslider.min.css';
+
 import '../scss/main.scss';
 import '../pug/components/form-elements/range-slider/slider';
 import '../pug/components/form-elements/masked-text-field/masked-field';
 import '../pug/components/form-elements/expandable-list/expandable-list';
 import '../pug/components/form-elements/dropdown/dropdown';
 import '../pug/components/form-elements/datepicker/datepicker';
+
+import '../pug/components/cards/room/room';
---
+++ src/pug/components/cards/room/room.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  $('#room-slider').bxSlider();
+});
