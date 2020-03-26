5f56c5472931d54b7763bc9cd1501a49b0e6d929
Frostmourne <cozroman@yandex.ru>
2020-03-26 23:39:50 +0300

    feat: add basic date filter

--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,8 @@ import '../pug/components/form-elements/datepicker/bootstrap-datepicker';
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.ru.min';
 import 'bootstrap-datepicker/dist/css/bootstrap-datepicker3.min.css';
 import 'bootstrap-datepicker/dist/css/bootstrap-datepicker3.standalone.css';
-import 'daterangepicker/daterangepicker';
-import 'daterangepicker/daterangepicker.css';
+// import 'daterangepicker/daterangepicker';
+// import 'daterangepicker/daterangepicker.css';
 import 'bxslider/dist/jquery.bxslider.min';
 import 'bxslider/dist/jquery.bxslider.min.css';
 
@@ -15,5 +15,6 @@ import '../pug/components/form-elements/masked-text-field/masked-field';
 import '../pug/components/form-elements/expandable-list/expandable-list';
 import '../pug/components/form-elements/dropdown/dropdown';
 import '../pug/components/form-elements/datepicker/datepicker';
+import '../pug/components/form-elements/date-filter/date-filter';
 
 import '../pug/components/cards/room/room';
---
+++ src/pug/components/form-elements/date-filter/date-filter.js
@@ -0,0 +1,6 @@
+import 'air-datepicker/dist/js/datepicker';
+import 'air-datepicker/dist/css/datepicker.css';
+
+$('.date-filter__input').datepicker({
+  dateFormat: 'dd M',
+});
