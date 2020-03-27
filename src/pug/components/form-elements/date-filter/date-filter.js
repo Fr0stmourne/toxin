fb81b46a8f26faaf150b9487c2349717209c55bb
Frostmourne <cozroman@yandex.ru>
2020-03-27 14:47:06 +0300

    feat: add date filter

--- src/pug/components/form-elements/date-filter/date-filter.js
+++ src/pug/components/form-elements/date-filter/date-filter.js
@@ -1,6 +1,19 @@
 import 'air-datepicker/dist/js/datepicker';
 import 'air-datepicker/dist/css/datepicker.css';
 
-$('.date-filter__input').datepicker({
-  dateFormat: 'dd M',
+$(() => {
+  $('.date-filter__input').datepicker({
+    dateFormat: 'dd M',
+    clearButton: true,
+    navTitles: {
+      days: 'MM yyyy',
+    },
+    // inline: true,
+    onShow: dp => {
+      dp.$datepicker.find('.datepicker--button[data-action="close"]').click(() => {
+        dp.hide();
+      });
+    },
+  });
+  $('.datepicker--buttons').append('<span class="datepicker--button" data-action="close">применить</span>');
 });
