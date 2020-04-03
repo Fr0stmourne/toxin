f86d969a4226765cdc279378be82930bc5d79353
Frostmourne <cozroman@yandex.ru>
2020-04-04 00:33:01 +0300

    feat: add some pp cards to cards.pug

--- src/pug/components/form-elements/datepicker/datepicker.js
+++ src/pug/components/form-elements/datepicker/datepicker.js
@@ -19,12 +19,36 @@ $('#booking-datepicker').datepicker({
   language: 'ru-RU',
 });
 
-$('#cards__start').datepicker({ 
-  onSelect: function (fd, d, picker) { 
-    $("#cards__start").val(fd.split("-")[0]);
-    $("#cards__end").val(fd.split("-")[1]);
-  }
-});
+function addRangeDatepicker() {
+
+  // const datepickerStartClass = '.js-range-datepicker-start';
+  // const datepickerEndClass = '.js-range-datepicker-end';
+  // const startInput =  $(datepickerStartClass).parent('.datepicker-double').find(datepickerStartClass);
+  // const endInput =  $(datepickerStartClass).parent('.datepicker-double').find(datepickerEndClass);
+
+
+  // $(startInput).datepicker({ 
+  //   onSelect: function (fd, d, picker) { 
+  //     $(startInput).val(fd.split("-")[0]);
+  //     $(endInput).val(fd.split("-")[1]);
+  //   }});
+  
+  $('#cards__start').datepicker({ 
+    onSelect: function (fd, d, picker) { 
+      $("#cards__start").val(fd.split("-")[0]);
+      $("#cards__end").val(fd.split("-")[1]);
+    }
+  });
+
+  $('#cards__start-1').datepicker({ 
+    onSelect: function (fd, d, picker) { 
+      $("#cards__start-1").val(fd.split("-")[0]);
+      $("#cards__end-1").val(fd.split("-")[1]);
+    }
+  });
+}
+
+addRangeDatepicker();
 
 // $(() => {
 //   $('.datepicker').daterangepicker({ linkedCalendars: false });
