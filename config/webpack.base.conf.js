b6feb6119d548cc0b99afcf9de986c93ca26c218
Frostmourne <cozroman@yandex.ru>
2020-05-09 17:28:24 +0300

    refactor: optimize pug files compilation and add tinypng compression for jpg images

--- config/webpack.base.conf.js
+++ config/webpack.base.conf.js
@@ -1,11 +1,11 @@
 const path = require('path');
-const fs = require('fs');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const TinyPngWebpackPlugin = require('tinypng-webpack-plugin');
 const glob = require('glob');
 
 const PATHS = {
@@ -14,14 +14,7 @@ const PATHS = {
   assets: 'assets/',
 };
 
-const PAGES_DIR = `${PATHS.src}/`;
-// let PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'));
-let PAGES = [];
-glob(`${__dirname}/../src/pages/**/*.pug`, {}, (err, files) => {
-  console.warn('ФАЙЛЫ', files);
-
-  PAGES = files;
-});
+const PAGES = glob.sync(`${__dirname}/../src/pages/**/*.pug`);
 
 module.exports = {
   // BASE config
@@ -77,7 +70,6 @@ module.exports = {
         test: /\.module\.s(a|c)ss$/,
         use: [
           this.mode === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
-          // MiniCssExtractPlugin.loader,
           {
             loader: 'css-loader',
             options: {
@@ -162,10 +154,6 @@ module.exports = {
       'window.$': 'jquery',
     }),
     new CopyWebpackPlugin([
-      // {
-      //   from: `${PATHS.src}/img`,
-      //   to: `${PATHS.assets}img`,
-      // },
       {
         from: `${PATHS.src}/fonts`,
         to: `${PATHS.assets}fonts`,
@@ -174,61 +162,21 @@ module.exports = {
         from: `${PATHS.src}/components`,
         to: `${PATHS.assets}components`,
       },
-      // {
-      //   test: /\.(jpe?g|png|gif|svg)/,
-      //   to: `${PATHS.assets}img`,
-      // },
     ]),
-    // ...PAGES.map(
-    //   page =>
-    //     new HtmlWebpackPlugin({
-    //       template: `.${page.split('..')[1]}`,
-    //       filename: `${page
-    //         .split('/')
-    //         .slice(-1)[0]
-    //         .replace(/\.pug/, '.html')}`,
-    //     }),
-    // ),
-    new HtmlWebpackPlugin({
-      template: `./src/pages/index/index.pug`,
-      filename: `index.html`,
+    ...PAGES.map(
+      page =>
+        new HtmlWebpackPlugin({
+          template: `.${page.split('..')[1]}`,
+          filename: `${page
+            .split('/')
+            .slice(-1)[0]
+            .replace(/\.pug/, '.html')}`,
+        }),
+    ),
+    new ImageminPlugin({ test: /\.(jpeg|jpg|png|gif|svg)$/i }),
+    new TinyPngWebpackPlugin({
+      ext: ['png', 'jpeg', 'jpg'],
+      key: 'RsCZev1geyFwOKznstLNGmxugsTZZmG6',
     }),
-    new HtmlWebpackPlugin({
-      template: `./src/pages/colors-type/colors-type.pug`,
-      filename: `colors-type.html`,
-    }),
-    new HtmlWebpackPlugin({
-      template: `./src/pages/cards/cards.pug`,
-      filename: `cards.html`,
-    }),
-    new HtmlWebpackPlugin({
-      template: `./src/pages/form-elements/form-elements.pug`,
-      filename: `form-elements.html`,
-    }),
-    new HtmlWebpackPlugin({
-      template: `./src/pages/headers-footers/headers-footers.pug`,
-      filename: `headers-footers.html`,
-    }),
-    new HtmlWebpackPlugin({
-      template: `./src/pages/landing-page/landing-page.pug`,
-      filename: `landing-page.html`,
-    }),
-    new HtmlWebpackPlugin({
-      template: `./src/pages/registration-page/registration-page.pug`,
-      filename: `registration-page.html`,
-    }),
-    new HtmlWebpackPlugin({
-      template: `./src/pages/room-details/room-details.pug`,
-      filename: `room-details.html`,
-    }),
-    new HtmlWebpackPlugin({
-      template: `./src/pages/search-room/search-room.pug`,
-      filename: `search-room.html`,
-    }),
-    new HtmlWebpackPlugin({
-      template: `./src/pages/signin-page/signin-page.pug`,
-      filename: `signin-page.html`,
-    }),
-    new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }),
   ],
 };
