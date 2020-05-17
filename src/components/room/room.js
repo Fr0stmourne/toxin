968d550678f811cdaf8a791c89e67756e11b23d5
Frostmourne <cozroman@yandex.ru>
2020-05-17 12:49:46 +0300

    #14

--- src/components/room/room.js
+++ src/components/room/room.js
@@ -1,3 +1,6 @@
+import 'bxslider/dist/jquery.bxslider.min';
+import 'bxslider/dist/jquery.bxslider.min.css';
+
 $(() => {
   $('.js-room-slider').bxSlider();
 });
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import 'normalize.css';
 import 'jquery/dist/jquery';
 import 'webpack-jquery-ui';
-import 'bxslider/dist/jquery.bxslider.min';
-import 'bxslider/dist/jquery.bxslider.min.css';
 
 function importAll(r) {
   r.keys().forEach(r);
 }
 
-importAll(require.context('./', true, /\.scss$/));
 importAll(require.context('./components/', true, /\.js$/));
 importAll(require.context('./pages/', true, /\.js$/));
+importAll(require.context('./', true, /\.scss$/));
 importAll(require.context('./', true, /\.(jpeg|jpg|png|gif|svg)$/));
