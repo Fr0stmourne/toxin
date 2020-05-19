6129b4bb83570e647ea44060b4403b17a30ecfda
Frostmourne <cozroman@yandex.ru>
2020-05-19 17:54:18 +0300

    #1 structure refactoring

---
+++ src/components/dropdown/vendors/dropdown.js
@@ -0,0 +1,104 @@
+/* eslint-disable */
+
+(function($) {
+  $.fn.htmlNumberSpinner = function() {
+    /* creating the counter buttons */
+    $(this).append(
+      "<button class='plus-minus__btn js-plus-minus-btn plus-minus__btn_minus js-minus-btn'>-</button> <input class='plus-minus__input' type='number'/> <button class='plus-minus__btn js-plus-minus-btn plus-minus__btn_plus js-plus-btn'>+</button>",
+    );
+
+    /* default value and variables and jquery elements */
+    const defaultValue = 0;
+    let inputValue;
+    const numberInput$ = $(this).find('.plus-minus__input');
+    const incrementerEl$ = $(this).find('.plus-minus__btn_plus');
+    const decrementerEl$ = $(this).find('.plus-minus__btn_minus');
+
+    /* hide the default number input spinner */
+    $(this).append(
+      '<style>' +
+        'input[type=number]::-webkit-inner-spin-button, \n' +
+        'input[type=number]::-webkit-outer-spin-button { \n' +
+        '    -webkit-appearance: none;\n' +
+        '    -moz-appearance: none;\n' +
+        '    appearance: none;\n' +
+        '    margin: 0; \n' +
+        '}</style>',
+    );
+
+    /* props - dynamic attributes */
+    const minAttributeValue = $(this).attr('min');
+    const maxAttributeValue = $(this).attr('max');
+    const stepAttributeValue = $(this).attr('step');
+
+    if (minAttributeValue) {
+      numberInput$.attr('min', +minAttributeValue);
+    }
+
+    if (maxAttributeValue) {
+      numberInput$.attr('max', +maxAttributeValue);
+    }
+
+    if (stepAttributeValue) {
+      numberInput$.attr('step', +stepAttributeValue);
+    }
+
+    /* set the default value into the input */
+    inputValue = minAttributeValue || defaultValue;
+    numberInput$.val(inputValue);
+
+    /* incrementer functionality */
+    incrementerEl$.click(function() {
+      const parentEl = $(this).parent();
+      inputValue = parentEl.find('.plus-minus__input').val();
+      if (maxAttributeValue) {
+        if (maxAttributeValue == inputValue) {
+          return;
+        }
+      }
+      if (stepAttributeValue) {
+        inputValue = parentEl.find('.plus-minus__input').val();
+        parentEl.find('.plus-minus__input').val(+inputValue + +stepAttributeValue);
+        return;
+      }
+      inputValue = parentEl.find('.plus-minus__input').val();
+      parentEl.find('.plus-minus__input').val(++inputValue);
+    });
+
+    /* decrementer functionality */
+    decrementerEl$.click(function() {
+      const parentEl = $(this).parent();
+      inputValue = parentEl.find('.plus-minus__input').val();
+      if (minAttributeValue) {
+        if (minAttributeValue == inputValue) {
+          return;
+        }
+      }
+      if (stepAttributeValue) {
+        inputValue = parentEl.find('.plus-minus__input').val();
+        parentEl.find('.plus-minus__input').val(+inputValue - +stepAttributeValue);
+        return;
+      }
+      inputValue = parentEl.find('.plus-minus__input').val();
+      parentEl.find('.plus-minus__input').val(--inputValue);
+    });
+
+    numberInput$.change(function() {
+      if (!maxAttributeValue || !minAttributeValue) return;
+      const currentValue = $(this).val();
+      if (+currentValue > +maxAttributeValue) {
+        $(this).val(maxAttributeValue);
+        return;
+      }
+      if (+currentValue < +minAttributeValue) {
+        $(this).val(minAttributeValue);
+      }
+    });
+  };
+
+  $.fn.getSpinnerValue = function() {
+    return $(this)
+      .find('.plus-minus__input')
+      .val();
+  };
+})(jQuery);
