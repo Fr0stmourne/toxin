598bf68d6f58e71a4115e3cf4b420ad751a54ab4
Frostmourne <cozroman@yandex.ru>
2020-06-01 11:35:51 +0300

    #22 webpack config improvements

--- config/webpack.build.conf.js
+++ config/webpack.build.conf.js
@@ -1,6 +1,8 @@
 const merge = require('webpack-merge');
 const TinyPngWebpackPlugin = require('tinypng-webpack-plugin');
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const baseWebpackConfig = require('./webpack.base.conf');
 
@@ -14,6 +16,19 @@ const buildWebpackConfig = merge(build, {
   mode: 'production',
   output: {
     publicPath: './',
+  },
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin({
+        cssProcessorPluginOptions: {
+          preset: ['default', { discardComments: { removeAll: true } }],
+        },
+        canPrint: true,
+      }),
+      new TerserPlugin({
+        parallel: true,
+      })
+    ],
   },
   plugins: [
     new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }),
